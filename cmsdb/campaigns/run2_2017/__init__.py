fc96c4aae19486f3d3ce055bc3ebde3ac2dbca54
Mathis Frahm <mathisfrahm@gmx.de>
2022-08-29 10:20:39 +0200

    lint

--- cmsdb/campaigns/run2_2017/__init__.py
+++ cmsdb/campaigns/run2_2017/__init__.py
@@ -7,9 +7,7 @@ See https://python-order.readthedocs.io/en/latest/quickstart.html#analysis-campa
 Dataset ids are identical to those in DAS (https://cmsweb.cern.ch/das).
 """
 
-from order import Campaign, DatasetInfo
-
-import cmsdb.processes as procs
+from order import Campaign
 
 #
 # campaign
