3d5c5dc73c5ffde10683cb222250214ced3a7de2
apaasch <alexanderpaa94@gmail.com>
2023-06-07 14:56:03 +0200

    Add multiple arguments

--- GetDASinfo.py
+++ GetDASinfo.py
@@ -7,50 +7,54 @@ from __future__ import annotations
 
 import subprocess
 import json
-from optparse import OptionParser
+from argparse import ArgumentParser
 import law
 
-def get_das_info(das_string: str, keys_of_interest: tuple | None = None):
+# def get_das_info(das_string: str, keys_of_interest: tuple | None = None):
+def get_das_info(das_strings: list[str], keys_of_interest: tuple | None = None):
+    for das_string in das_strings:
+        # set default keys of interest
+        keys_of_interest = keys_of_interest or (
+            "name", "dataset_id", "nfiles", "nevents",
+        )
 
-    # set default keys of interest
-    keys_of_interest = keys_of_interest or (
-        "name", "dataset_id", "nfiles", "nevents",
-    )
+        # call dasgoclient command
+        cmd = f"dasgoclient -query='dataset={das_string}' -json"
+        code, out, _ = law.util.interruptable_popen(
+            cmd,
+            shell=True,
+            stdout=subprocess.PIPE,
+            executable="/bin/bash",
+        )
 
-    # call dasgoclient command
-    cmd = f"dasgoclient -query='dataset={das_string}' -json"
-    code, out, _ = law.util.interruptable_popen(
-        cmd,
-        shell=True,
-        stdout=subprocess.PIPE,
-        executable="/bin/bash",
-    )
+        if code != 0:
+            raise Exception(f"dasgoclient query failed:\n{out}")
 
-    if code != 0:
-        raise Exception(f"dasgoclient query failed:\n{out}")
-
-    infos = json.loads(out)
-    # json file from dataset contains 4 dictonaries, containing different infos (ids, files, etc.)
-    info_of_interest = {'name': das_string}
-    for info in infos:
-        dataset_info = info["dataset"][0]
-    
-        # check that all keys of interest are there
-        # if not set(keys_of_interest).issubset(dataset_info.keys()):
-        #     continue
+        infos = json.loads(out)
+        # json file from dataset contains 4 dictonaries, containing different infos (ids, files, etc.)
+        info_of_interest = {'name': das_string}
+        for info in infos:
+            dataset_info = info["dataset"][0]
         
-        if "dataset_info" in info["das"]["services"][0]:
-            info_of_interest["dataset_id"] = dataset_info.get("dataset_id", "")
-        elif "filesummaries" in info["das"]["services"][0]:
-            info_of_interest["nfiles"] = dataset_info.get("nfiles", "")
-            info_of_interest["nevents"] = dataset_info.get("nevents", "")
-    
-    print(json.dumps(info_of_interest, indent=4))
+            # check that all keys of interest are there
+            # if not set(keys_of_interest).issubset(dataset_info.keys()):
+            #     continue
+            
+            if "dataset_info" in info["das"]["services"][0]:
+                info_of_interest["dataset_id"] = dataset_info.get("dataset_id", "")
+            elif "filesummaries" in info["das"]["services"][0]:
+                info_of_interest["nfiles"] = dataset_info.get("nfiles", "")
+                info_of_interest["nevents"] = dataset_info.get("nevents", "")
+        
+        print(json.dumps(info_of_interest, indent=4))
 
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option('-d', '--dataset', dest='dataset', help='das name')
-    (options,args) = parser.parse_args()
+    parser = ArgumentParser()
+    # parser.add_option('-d', '--dataset', dest='dataset', nargs='+', help='das name')
+    parser.add_argument('-d', '--dataset', dest='dataset', nargs='+', help='das name')
+    # (options,args) = parser.parse_args()
+    args = parser.parse_args()
 
-    get_das_info(options.dataset)
+    # get_das_info(options.dataset)
+    get_das_info(args.dataset)
