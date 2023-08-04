8e0784a9376de21fa064cfef05ad1b2736330b90
apaasch <alexanderpaa94@gmail.com>
2023-08-04 08:56:21 +0200

    reduce characters in line

--- cmsdb/campaigns/run2_2017_JMEnano_v9/qcd.py
+++ cmsdb/campaigns/run2_2017_JMEnano_v9/qcd.py
@@ -14,12 +14,15 @@ from cmsdb.campaigns.run2_2017_JMEnano_v9 import campaign_run2_2017_JMEnano_v9 a
 
 # HT-binned samples
 
+generator = "TuneCP5_PSWeights_13TeV-madgraph-pythia8"
+cmssw = "106X_mc2017_realistic_v9-v1"
+
 cpn.add_dataset(
     name="qcd_ht50to100_madgraph",
     id=14281497,
     processes=[procs.qcd_ht50to100],
     keys=[
-        "/QCD_HT50to100_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT50to100_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=52,
     n_events=26129826,
@@ -30,7 +33,7 @@ cpn.add_dataset(
     id=14300506,
     processes=[procs.qcd_ht100to200],
     keys=[
-        "/QCD_HT100to200_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT100to200_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=99,
     n_events=54705747,
@@ -41,7 +44,7 @@ cpn.add_dataset(
     id=14289012,
     processes=[procs.qcd_ht200to300],
     keys=[
-        "/QCD_HT200to300_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT200to300_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=85,
     n_events=42679193,
@@ -52,7 +55,7 @@ cpn.add_dataset(
     id=14288347,
     processes=[procs.qcd_ht300to500],
     keys=[
-        "/QCD_HT300to500_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT300to500_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=94,
     n_events=43589739,
@@ -63,7 +66,7 @@ cpn.add_dataset(
     id=14279003,
     processes=[procs.qcd_ht500to700],
     keys=[
-        "/QCD_HT500to700_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT500to700_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=85,
     n_events=36082941,
@@ -74,7 +77,7 @@ cpn.add_dataset(
     id=14281356,
     processes=[procs.qcd_ht700to1000],
     keys=[
-        "/QCD_HT700to1000_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT700to1000_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=84,
     n_events=32788396,
@@ -85,7 +88,7 @@ cpn.add_dataset(
     id=14296775,
     processes=[procs.qcd_ht1000to1500],
     keys=[
-        "/QCD_HT1000to1500_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT1000to1500_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=65,
     n_events=10256089,
@@ -96,7 +99,7 @@ cpn.add_dataset(
     id=14270490,
     processes=[procs.qcd_ht1500to2000],
     keys=[
-        "/QCD_HT1500to2000_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT1500to2000_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=43,
     n_events=7540039,
@@ -107,7 +110,7 @@ cpn.add_dataset(
     id=14296752,
     processes=[procs.qcd_ht2000],
     keys=[
-        "/QCD_HT2000toInf_TuneCP5_PSWeights_13TeV-madgraph-pythia8/RunIISummer20UL17NanoAODv9-20UL17JMENano_106X_mc2017_realistic_v9-v1/NANOAODSIM",
+        f"/QCD_HT2000toInf_{generator}/RunIISummer20UL17NanoAODv9-20UL17JMENano_{cmssw}/NANOAODSIM",
     ],
     n_files=38,
     n_events=4055223,
