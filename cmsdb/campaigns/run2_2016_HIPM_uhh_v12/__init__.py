baf429d11e8e43311fe9777261196348f3a44fcd
Ana A <ana.rita.alves.andrade@cern.ch>
2024-01-25 12:21:26 +0100

    fixing various typos

--- cmsdb/campaigns/run2_2016_HIPM_uhh_v12/__init__.py
+++ cmsdb/campaigns/run2_2016_HIPM_uhh_v12/__init__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
 """
-Common, analysis independent definition of the 2017 data-taking campaign
-with datasets at NanoAOD tier in version 11, created with custom content at UHH.
+Common, analysis independent definition of the 2016 data-taking campaign
+with datasets at NanoAOD tier in version 12, created with custom content at UHH.
 See https://python-order.readthedocs.io/en/latest/quickstart.html#analysis-campaign-and-config.
 
 Dataset ids are identical to those of MiniAOD input datasets in DAS (https://cmsweb.cern.ch/das).
@@ -34,16 +34,22 @@ campaign_run2_2016_HIMP_uhh_v12 = Campaign(
         "custom": {
             "name": "run2_2016_HIMP_nano_uhh_v12",
             "creator": "uhh",
-            "location": "davs://dcache-cms-webdav-wan.desy.de:2880/pnfs/desy.de/cms/tier2/store/user/bwieders/nano_uhh_v11/merged_2048.0MB",  # noqa
+            "location": "davs://dcache-cms-webdav-wan.desy.de:2880/pnfs/desy.de/cms/tier2/store/user/aalvesan/nano_uhh_v12_2016_HIPM/merged_2048.0MB",  # noqa
         },
     },
 )
 
-
 # trailing imports to load datasets
 import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.data  # noqa
-import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.top  # noqa
+import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.DY  # noqa
 import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.ewk  # noqa
-import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.qcd  # noqa
-import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.higgs  # noqa
 import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.hh2bbtautau  # noqa
+import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.MultiBoson  # noqa
+import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.SingleHiggs  # noqa
+import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.SingleTop  # noqa
+import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.ttbar  # noqa
+import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.W  # noqa
+import cmsdb.campaigns.run2_2016_HIMP_nano_uhh_v12.Z  # noqa
+
+
+
