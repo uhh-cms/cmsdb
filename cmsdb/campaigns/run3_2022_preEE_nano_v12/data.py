d62b7a0257318a24d3d738bc3e70748b7aa2f24c
Mathis Frahm <mathisfrahm@gmx.de>
2024-01-31 17:09:02 +0100

    implement main hh(bbww) backgrounds in 2022_preEE_nano_v12 campaign

---
+++ cmsdb/campaigns/run3_2022_preEE_nano_v12/data.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+"""
+CMS datasets from the 2022 pre-EE data-taking campaign
+"""
+
+# import cmsdb.processes as procs
+# from cmsdb.campaigns.run3_2022_preEE_nano_v12 import campaign_run3_2022_preEE_nano_v12 as cpn
+
+# TODO
