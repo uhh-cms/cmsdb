added2f22f6c0e68e3583676aa071cc05bc1c077
Mathis Frahm <mathisfrahm@gmx.de>
2024-02-01 10:18:52 +0100

    add hh(bbww) datasets to 2022 nano v12 campaigns

--- cmsdb/campaigns/run3_2022_postEE_nano_v12/__init__.py
+++ cmsdb/campaigns/run3_2022_postEE_nano_v12/__init__.py
@@ -39,3 +39,4 @@ import cmsdb.campaigns.run3_2022_postEE_nano_v12.data  # noqa
 import cmsdb.campaigns.run3_2022_postEE_nano_v12.top  # noqa
 import cmsdb.campaigns.run3_2022_postEE_nano_v12.ewk  # noqa
 import cmsdb.campaigns.run3_2022_postEE_nano_v12.qcd  # noqa
+import cmsdb.campaigns.run3_2022_postEE_nano_v12.hh2bbww  # noqa
