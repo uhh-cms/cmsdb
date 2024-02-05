7c5133e689f573f37cff62a442c1656f467849e0
Mathis Frahm <mathisfrahm@gmx.de>
2024-02-05 14:33:18 +0100

    add 2022 muon and egamma datsets

--- cmsdb/campaigns/run3_2022_preEE_nano_v12/data.py
+++ cmsdb/campaigns/run3_2022_preEE_nano_v12/data.py
@@ -4,7 +4,107 @@
 CMS datasets from the 2022 pre-EE data-taking campaign
 """
 
-# import cmsdb.processes as procs
-# from cmsdb.campaigns.run3_2022_preEE_nano_v12 import campaign_run3_2022_preEE_nano_v12 as cpn
+import cmsdb.processes as procs
+from cmsdb.campaigns.run3_2022_preEE_nano_v12 import campaign_run3_2022_preEE_nano_v12 as cpn
 
-# TODO
+
+#
+# Muon
+#
+
+
+cpn.add_dataset(
+    name="data_mu_c",
+    id=14784127,
+    is_data=True,
+    processes=[procs.data_mu],
+    keys=[
+        "/Muon/Run2022C-22Sep2023-v1/NANOAOD",  # noqa
+    ],
+    n_files=124,
+    n_events=138427345,
+    aux={
+        "era": "C",
+    },
+)
+
+cpn.add_dataset(
+    name="data_mu_d",
+    id=14783357,
+    is_data=True,
+    processes=[procs.data_mu],
+    keys=[
+        "/Muon/Run2022D-22Sep2023-v1/NANOAOD",  # noqa
+    ],
+    n_files=82,
+    n_events=75468381,
+    aux={
+        "era": "D",
+    },
+)
+
+
+#
+# E/Gamma
+#
+
+
+cpn.add_dataset(
+    name="data_egamma_a",
+    id=14783268,
+    is_data=True,
+    processes=[procs.data_egamma],
+    keys=[
+        "/EGamma/Run2022A-22Sep2023-v1/NANOAOD",  # noqa
+    ],
+    n_files=5,
+    n_events=186,
+    aux={
+        "era": "A",
+    },
+)
+
+cpn.add_dataset(
+    name="data_egamma_b",
+    id=14826835,
+    is_data=True,
+    processes=[procs.data_egamma],
+    keys=[
+        "/EGamma/Run2022B-22Sep2023-v2/NANOAOD",  # noqa
+    ],
+    n_files=14,
+    n_events=11074301,
+    aux={
+        "era": "B",
+    },
+)
+
+cpn.add_dataset(
+    name="data_egamma_c",
+    id=14784140,
+    is_data=True,
+    processes=[procs.data_egamma],
+    keys=[
+        "/EGamma/Run2022C-22Sep2023-v1/NANOAOD",  # noqa
+    ],
+    n_files=313,
+    n_events=263689151,
+    aux={
+        "era": "C",
+    },
+)
+
+cpn.add_dataset(
+    name="data_egamma_d",
+    id=14783299,
+    is_data=True,
+    processes=[procs.data_egamma],
+    keys=[
+        "/EGamma/Run2022D-22Sep2023-v1/NANOAOD",  # noqa
+    ],
+    n_files=109,
+    n_events=89134996,
+    aux={
+        "era": "D",
+    },
+)
