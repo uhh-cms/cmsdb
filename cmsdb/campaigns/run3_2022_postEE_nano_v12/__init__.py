549bb7af24b8b4f9c43a0cf16fca7641581a34e9
Thanh Tan Nguyen <nguyenth@naf-cms17.desy.de>
2024-05-06 14:28:54 +0200

    examples for hhh datasets for 2022 postEE

--- cmsdb/campaigns/run3_2022_postEE_nano_v12/__init__.py
+++ cmsdb/campaigns/run3_2022_postEE_nano_v12/__init__.py
@@ -40,3 +40,5 @@ import cmsdb.campaigns.run3_2022_postEE_nano_v12.top  # noqa
 import cmsdb.campaigns.run3_2022_postEE_nano_v12.ewk  # noqa
 import cmsdb.campaigns.run3_2022_postEE_nano_v12.qcd  # noqa
 import cmsdb.campaigns.run3_2022_postEE_nano_v12.hh2bbww  # noqa
+import cmsdb.campaigns.run3_2022_postEE_nano_v12.hhh4b2tau  # noqa
+
---
+++ cmsdb/campaigns/run3_2022_postEE_nano_v12/hhh4b2tau.py
@@ -0,0 +1,15 @@
+
+import cmsdb.processes as procs
+from cmsdb.campaigns.run3_2022_postEE_nano_v12 import campaign_run3_2022_postEE_nano_v12 as cpn
+
+
+cpn.add_dataset(
+    name="hhh4b2tau_c3_0_d4_0_madgraph",
+    id=14792106,
+    processes=[procs.hhh_ggf_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-0_d4-0_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=89,
+    n_events=33550615,
+)
