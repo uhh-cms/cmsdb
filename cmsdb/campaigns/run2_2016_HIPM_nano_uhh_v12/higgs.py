4fba9d636f77a527e78db7fd06475634ed497191
Marcel Rieger <riga@users.noreply.github.com>
2024-05-24 17:58:12 +0200

    Please the linter.

--- cmsdb/campaigns/run2_2016_HIPM_nano_uhh_v12/higgs.py
+++ cmsdb/campaigns/run2_2016_HIPM_nano_uhh_v12/higgs.py
@@ -10,97 +10,97 @@ from cmsdb.campaigns.run2_2016_HIPM_nano_uhh_v12 import campaign_run2_2016_HIPM_
 
 # ggf
 
-#cpn.add_dataset(
-#    name="h_ggf_tautau_powheg",
-#    id=14314017,
-#    processes=[procs.h_ggf_tautau],
-#    keys=[
-#        "/GluGluHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
-#    ],
-#    n_files=4,
-#    n_events=6430000,
-#)
+# cpn.add_dataset(
+#     name="h_ggf_tautau_powheg",
+#     id=14314017,
+#     processes=[procs.h_ggf_tautau],
+#     keys=[
+#         "/GluGluHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
+#     ],
+#     n_files=4,
+#     n_events=6430000,
+# )
 
 # vbf
 
-#cpn.add_dataset(
-#    name="h_vbf_tautau_powheg",
-#    id=14346935,
-#    processes=[procs.h_vbf_tautau],
-#    keys=[
-#        "/VBFHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
-#    ],
-#    n_files=2,
-#    n_events=1395000,
-#)
+# cpn.add_dataset(
+#     name="h_vbf_tautau_powheg",
+#     id=14346935,
+#     processes=[procs.h_vbf_tautau],
+#     keys=[
+#         "/VBFHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
+#     ],
+#     n_files=2,
+#     n_events=1395000,
+# )
 
 # H radiation
 
-#cpn.add_dataset(
-#    name="zh_tautau_powheg",
-#    id=14284521,
-#    processes=[procs.zh_tautau],
-#    keys=[
-#        "/ZHToTauTau_M125_CP5_13TeV-powheg-pythia8_ext1/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
-#    ],
-#    n_files=2,
-#    n_events=2481740,
-#)
+# cpn.add_dataset(
+#     name="zh_tautau_powheg",
+#     id=14284521,
+#     processes=[procs.zh_tautau],
+#     keys=[
+#         "/ZHToTauTau_M125_CP5_13TeV-powheg-pythia8_ext1/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
+#     ],
+#     n_files=2,
+#     n_events=2481740,
+# )
 
-#cpn.add_dataset(
-#    name="wph_tautau_powheg",
-#    id=14337370,
-#    processes=[procs.wph_tautau],
-#    keys=[
-#        "/WplusHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
-#    ],
-#    n_files=2,
-#    n_events=1915820,
-#)
+# cpn.add_dataset(
+#     name="wph_tautau_powheg",
+#     id=14337370,
+#     processes=[procs.wph_tautau],
+#     keys=[
+#         "/WplusHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
+#     ],
+#     n_files=2,
+#     n_events=1915820,
+# )
 
-#cpn.add_dataset(
-#    name="wmh_tautau_powheg",
-#    id=14335155,
-#    processes=[procs.wmh_tautau],
-#    keys=[
-#        "/WminusHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
-#    ],
-#    n_files=2,
-#    n_events=1976651,
-#)
+# cpn.add_dataset(
+#     name="wmh_tautau_powheg",
+#     id=14335155,
+#     processes=[procs.wmh_tautau],
+#     keys=[
+#         "/WminusHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
+#     ],
+#     n_files=2,
+#     n_events=1976651,
+# )
 
 
 # ttH
 
-#cpn.add_dataset(
-#    name="tth_tautau_powheg",
-#    id=14340599,
-#    processes=[procs.tth_tautau],
-#    keys=[
-#        "/ttHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
-#    ],
-#    n_files=15,
-#    n_events=10865700,
-#)
+# cpn.add_dataset(
+#     name="tth_tautau_powheg",
+#     id=14340599,
+#     processes=[procs.tth_tautau],
+#     keys=[
+#         "/ttHToTauTau_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
+#     ],
+#     n_files=15,
+#     n_events=10865700,
+# )
 
-#cpn.add_dataset(
-#    name="tth_nonbb_powheg",
-#    id=14278195,
-#    processes=[procs.tth_nonbb],
-#    keys=[
-#        "/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
-#    ],
-#    n_files=3,
-#    n_events=1977996,
-#)
+# cpn.add_dataset(
+#     name="tth_nonbb_powheg",
+#     id=14278195,
+#     processes=[procs.tth_nonbb],
+#     keys=[
+#         "/ttHToNonbb_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
+#     ],
+#     n_files=3,
+#     n_events=1977996,
+# )
 
-#cpn.add_dataset(
-#    name="tth_bb_powheg",
-#    id=14284890,
-#    processes=[procs.tth_bb],
-#    keys=[
-#        "/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
-#    ],
-#    n_files=6,
-#    n_events=4622000,
-#)
+# cpn.add_dataset(
+#     name="tth_bb_powheg",
+#     id=14284890,
+#     processes=[procs.tth_bb],
+#     keys=[
+#         "/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8/RunIISummer20UL16MiniAODAPVv2-106X_mcRun2_asymptotic_preVFP_v11-v2/NANOAODSIM",  # noqa
+#     ],
+#     n_files=6,
+#     n_events=4622000,
+# )
