253583aefc11aa2d378bdf470c263bd67b107be9
Thanh Tan Nguyen <nguyenth@naf-cms11.desy.de>
2024-06-04 15:43:18 +0200

    implement different coupling hypotheses for hhh

--- cmsdb/campaigns/run3_2022_preEE_nano_v12/hhh4b2tau.py
+++ cmsdb/campaigns/run3_2022_preEE_nano_v12/hhh4b2tau.py
@@ -4,12 +4,122 @@ from cmsdb.campaigns.run3_2022_preEE_nano_v12 import campaign_run3_2022_preEE_na
 
 # SM sample
 cpn.add_dataset(
-    name="hhh4b2tau_c3_0_d4_0_amcatnlo",
+    name="hhh_c3_0_d4_0_4b2tau_madgraph",
     id=14791536,
-    processes=[procs.hhh_ggf_4b2tau],
+    processes=[procs.hhh_c3_0_d4_0_4b2tau],
     keys=[
         "/HHHto4B2Tau_c3-0_d4-0_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
     ],
     n_files=32,
     n_events=9483880,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_0_d4_99_4b2tau_madgraph",
+    id=14804364,
+    processes=[procs.hhh_c3_0_d4_99_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-0_d4-99_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=18,
+    n_events=387652,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_0_d4_minus1_4b2tau_madgraph",
+    id=14796291,
+    processes=[procs.hhh_c3_0_d4_minus1_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-0_d4-minus1_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=20,
+    n_events=393610,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_19_d4_19_4b2tau_madgraph",
+    id=14805829,
+    processes=[procs.hhh_c3_19_d4_19_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-19_d4-19_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=18,
+    n_events=393190,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_1_d4_0_4b2tau_madgraph",
+    id=14805285,
+    processes=[procs.hhh_c3_1_d4_0_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-1_d4-0_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=16,
+    n_events=395878,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_1_d4_2_4b2tau_madgraph",
+    id=14797470,
+    processes=[procs.hhh_c3_1_d4_2_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-1_d4-2_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=18,
+    n_events=407634,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_2_d4_minus1_4b2tau_madgraph",
+    id=14798802,
+    processes=[procs.hhh_c3_2_d4_minus1_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-2_d4-minus1_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=21,
+    n_events=396595,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_4_d4_9_4b2tau_madgraph",
+    id=14885952,
+    processes=[procs.hhh_c3_4_d4_9_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-4_d4-9_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v3/NANOAODSIM",  # noqa
+    ],
+    n_files=14,
+    n_events=262292,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_minus1_d4_0_4b2tau_madgraph",
+    id=14797875,
+    processes=[procs.hhh_c3_minus1_d4_0_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-minus1_d4-0_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=22,
+    n_events=372600,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_minus1_d4_minus1_4b2tau_madgraph",
+    id=14794127,
+    processes=[procs.hhh_c3_minus1_d4_minus1_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-minus1_d4-minus1_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=17,
+    n_events=398634,
+)
+
+cpn.add_dataset(
+    name="hhh_c3_minus1p5_d4_minus0p5_4b2tau_madgraph",
+    id=14796572,
+    processes=[procs.hhh_c3_minus1p5_d4_minus0p5_4b2tau],
+    keys=[
+        "/HHHto4B2Tau_c3-minus1p5_d4-minus0p5_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=26,
+    n_events=406138,
 )
