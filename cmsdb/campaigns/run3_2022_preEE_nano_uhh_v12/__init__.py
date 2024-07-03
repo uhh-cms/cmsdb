ca5abce2996e4180a6c11171ad15c7d7a21f86f3
Marcel R. <github.riga@icloud.com>
2024-07-03 14:14:57 +0200

    Add missing imports.

--- cmsdb/campaigns/run3_2022_preEE_nano_uhh_v12/__init__.py
+++ cmsdb/campaigns/run3_2022_preEE_nano_uhh_v12/__init__.py
@@ -39,4 +39,6 @@ campaign_run3_2022_preEE_nano_uhh_v12 = Campaign(
 # trailing imports to load datasets
 import cmsdb.campaigns.run3_2022_preEE_nano_uhh_v12.data  # noqa
 import cmsdb.campaigns.run3_2022_preEE_nano_uhh_v12.top  # noqa
+import cmsdb.campaigns.run3_2022_preEE_nano_uhh_v12.ewk  # noqa
+import cmsdb.campaigns.run3_2022_preEE_nano_uhh_v12.higgs  # noqa
 import cmsdb.campaigns.run3_2022_preEE_nano_uhh_v12.hh2bbtautau  # noqa
