76b73a9545e1264534f7cd531985f28acbbbc93f
Norman Seeba <norman.seeba@gmail.com>
2024-09-03 13:25:13 +0200

    hhh4b2tau naming fixes

--- cmsdb/campaigns/run3_2023_postBPix_nano_v12/hhh4b2tau.py
+++ cmsdb/campaigns/run3_2023_postBPix_nano_v12/hhh4b2tau.py
@@ -1,16 +1,13 @@
 # coding: utf-8
 
-# Find information from here: https://docs.google.com/presentation/d/1TjPem5jX0fzqvTGl271_nQFoVBabsrdrO0i8Qo1uD5E/edit#slide=id.g289f499aa6b_2_58 
-
 import cmsdb.processes as procs
 from cmsdb.campaigns.run3_2023_postBPix_nano_v12 import campaign_run3_2023_postBPix_nano_v12 as cpn
 
 # SM sample
-
 cpn.add_dataset(
-    name="hhh4b2tau_c3_0_d4_0_madgraph",
+    name="hhh_4b2tau_c30_d40_amcatnlo",
     id=14846187,
-    processes=[procs.hhh_ggf_4b2tau],
+    processes=[procs.hhh_4b2tau_c30_d40],
     keys=[
         "/HHHto4B2Tau_c3-0_d4-0_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer23BPixNanoAODv12-130X_mcRun3_2023_realistic_postBPix_v2-v2/NANOAODSIM",  # noqa
     ],
