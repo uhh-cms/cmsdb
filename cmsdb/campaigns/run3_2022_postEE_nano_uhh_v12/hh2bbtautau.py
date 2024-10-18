0b9c1e68cf294c1bf212ff59c95780ee5133753d
Ana A <ana.rita.alves.andrade@cern.ch>
2024-10-18 17:12:35 +0200

    adding some ggf entries

--- cmsdb/campaigns/run3_2022_postEE_nano_uhh_v12/hh2bbtautau.py
+++ cmsdb/campaigns/run3_2022_postEE_nano_uhh_v12/hh2bbtautau.py
@@ -32,8 +32,112 @@ cpn.add_dataset(
 
 # different couplings, keeping c2 = 0
 
-# different bsm couplings, with c2 != 0
+cpn.add_dataset(
+    name="hh_ggf_hbb_htt_kl0_kt1_powheg",
+    # MISSING
+)
 
+cpn.add_dataset(
+    name="hh_ggf_hbb_htt_kl2p45_kt1_powheg",
+    id=14952632,
+    processes=[procs.hh_ggf_hbb_htt_kl2p45_kt1],
+    keys=[
+        "/GluGlutoHHto2B2Tau_kl-2p45_kt-1p00_c2-0p00_LHEweights_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22EEMiniAODv4_NanoAODv12UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=7,
+    n_events=3_435_020,
+    aux={
+        "merging_factors": {
+            "nominal": 61,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="hh_ggf_hbb_htt_kl5_kt1_powheg",
+    # MISSING
+)
+
+# different bsm couplings, with c2 != 0
+cpn.add_dataset(
+    name="hh_ggf_hbb_htt_kl0_kt1_c21_powheg",
+    id=14791033,
+    processes=[procs.hh_ggf_hbb_htt_kl0_kt1_c21],
+    keys=[
+        "/GluGlutoHHto2B2Tau_kl-0p00_kt-1p00_c2-1p00_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22EEMiniAODv4_NanoAODv12UHH-Poisson60KeepRAW_130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=1,
+    n_events=100_000,
+    aux={
+        "merging_factors": {
+            "nominal": 3,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="hh_ggf_hbb_htt_kl1_kt1_c20p10_powheg",
+    id=14800226,
+    processes=[procs.hh_ggf_hbb_htt_kl1_kt1_c20p10],
+    keys=[
+        "/GluGlutoHHto2B2Tau_kl-1p00_kt-1p00_c2-0p10_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22EEMiniAODv4_NanoAODv12UHH-Poisson60KeepRAW_130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=1,
+    n_events=95_965,
+    aux={
+        "merging_factors": {
+            "nominal": 3,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="hh_ggf_hbb_htt_kl1_kt1_c20p35_powheg",
+    id=14793318,
+    processes=[procs.hh_ggf_hbb_htt_kl1_kt1_c20p35],
+    keys=[
+        "/GluGlutoHHto2B2Tau_kl-1p00_kt-1p00_c2-0p35_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22EEMiniAODv4_NanoAODv12UHH-Poisson60KeepRAW_130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=1,
+    n_events=99_650,
+    aux={
+        "merging_factors": {
+            "nominal": 4,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="hh_ggf_hbb_htt_kl1_kt1_c23_powheg",
+    id=14807766,
+    processes=[procs.hh_ggf_hbb_htt_kl1_kt1_c23],
+    keys=[
+        "/GluGlutoHHto2B2Tau_kl-1p00_kt-1p00_c2-3p00_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22EEMiniAODv4_NanoAODv12UHH-Poisson60KeepRAW_130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=1,
+    n_events=99_068,
+    aux={
+        "merging_factors": {
+            "nominal": 4,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="hh_ggf_hbb_htt_kl1_kt1_c2m2_powheg",
+    id=14804196,
+    processes=[procs.hh_ggf_hbb_htt_kl1_kt1_c2m2],
+    keys=[
+        "/GluGlutoHHto2B2Tau_kl-1p00_kt-1p00_c2-m2p00_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22EEMiniAODv4_NanoAODv12UHH-Poisson60KeepRAW_130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=1,
+    n_events=96_178,
+    aux={
+        "merging_factors": {
+            "nominal": 4,
+        },
+    },
+)
 
 #
 # VBF -> H -> HH
