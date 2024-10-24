9112eef5dc890951730e2d5507c0de793dca2e51
Marcel Rieger <riga@users.noreply.github.com>
2024-10-24 13:56:58 +0200

    Remove unused import.

--- cmsdb/campaigns/run3_2022_postEE_nano_uhh_v12/data.py
+++ cmsdb/campaigns/run3_2022_postEE_nano_uhh_v12/data.py
@@ -4,8 +4,6 @@
 CMS datasets from the 2022 post-EE data-taking campaign
 """
 
-from order import DatasetInfo
-
 import cmsdb.processes as procs
 from cmsdb.campaigns.run3_2022_postEE_nano_uhh_v12 import campaign_run3_2022_postEE_nano_uhh_v12 as cpn
 
