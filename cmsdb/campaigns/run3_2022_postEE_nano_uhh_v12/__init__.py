4fe4a6d9eb0abe6d6bfe086793f24ea34d745e15
Marcel Rieger <riga@users.noreply.github.com>
2024-10-24 13:24:40 +0200

    Comment out missing datasets for now.

--- cmsdb/campaigns/run3_2022_postEE_nano_uhh_v12/__init__.py
+++ cmsdb/campaigns/run3_2022_postEE_nano_uhh_v12/__init__.py
@@ -40,6 +40,6 @@ campaign_run3_2022_postEE_nano_uhh_v12 = Campaign(
 # trailing imports to load datasets
 import cmsdb.campaigns.run3_2022_postEE_nano_uhh_v12.data  # noqa
 import cmsdb.campaigns.run3_2022_postEE_nano_uhh_v12.top  # noqa
-import cmsdb.campaigns.run3_2022_postEE_nano_uhh_v12.ewk  # noqa
-import cmsdb.campaigns.run3_2022_postEE_nano_uhh_v12.higgs  # noqa
+# import cmsdb.campaigns.run3_2022_postEE_nano_uhh_v12.ewk  # noqa
+# import cmsdb.campaigns.run3_2022_postEE_nano_uhh_v12.higgs  # noqa
 import cmsdb.campaigns.run3_2022_postEE_nano_uhh_v12.hh2bbtautau # noqa
