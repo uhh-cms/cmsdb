d7baf758ec68c9f7c37bb421c093948c753976f0
Anas Haddad <hhhaaanas@gmail.com>
2024-12-19 13:41:52 +0100

    added 2023pre di- and triple-boson entries

--- cmsdb/campaigns/run3_2023_preBPix_nano_uhh_v14/ewk.py
+++ cmsdb/campaigns/run3_2023_preBPix_nano_uhh_v14/ewk.py
@@ -5,7 +5,7 @@ Electroweak datasets for the 2023 preBPix data-taking campaign with datasets at 
 version 14, created with custom content at UHH.
 """
 
-# import cmsdb.processes as procs
+import cmsdb.processes as procs
 from cmsdb.campaigns.run3_2023_preBPix_nano_uhh_v14 import campaign_run3_2023_preBPix_nano_uhh_v14 as cpn  # noqa
 
 
@@ -44,10 +44,122 @@ from cmsdb.campaigns.run3_2023_preBPix_nano_uhh_v14 import campaign_run3_2023_pr
 # Di-boson
 #
 
+cpn.add_dataset(
+    name="zz_pythia",
+    id=14788274,
+    processes=[procs.zz],
+    keys=[
+        "/ZZ_TuneCP5_13p6TeV_pythia8/Run3Summer23MiniAODv4_NanoAODv14UHH-130X_mcRun3_2023_realistic_v14-v2/NANOAODSIM",
+    ],
+    n_files=3,
+    n_events=2_517_000,
+    aux={
+        "merging_factors": {
+            "nominal": 22,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="ww_pythia",
+    id=14784447,
+    processes=[procs.ww],
+    keys=[
+        "/WW_TuneCP5_13p6TeV_pythia8/Run3Summer23MiniAODv4_NanoAODv14UHH-130X_mcRun3_2023_realistic_v14-v2/NANOAODSIM",
+    ],
+    n_files=41,
+    n_events=33_507_000,
+    aux={
+        "merging_factors": {
+            "nominal": 16,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="wz_pythia",
+    id=14789732,
+    processes=[procs.wz],
+    keys=[
+        "/WZ_TuneCP5_13p6TeV_pythia8/Run3Summer23MiniAODv4_NanoAODv14UHH-130X_mcRun3_2023_realistic_v14-v2/NANOAODSIM",
+    ],
+    n_files=20,
+    n_events=16_770_000,
+    aux={
+        "merging_factors": {
+            "nominal": 16,
+        },
+    },
+)
+
 # tba
 
 #
 # Triple-boson
 #
+
+cpn.add_dataset(
+    name="www_4f_amcatnlo",
+    id=14808796,
+    processes=[procs.www],
+    keys=[
+        "/WWW_4F_TuneCP5_13p6TeV_amcatnlo-madspin-pythia8/Run3Summer23MiniAODv4_NanoAODv14UHH-130X_mcRun3_2023_realistic_v14-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=2,
+    n_events=936_000,
+    aux={
+        "merging_factors": {
+            "nominal": 13,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="wzz_amcatnlo",
+    id=14808744,
+    processes=[procs.wzz],
+    keys=[
+        "/WZZ_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer23MiniAODv4_NanoAODv14UHH-130X_mcRun3_2023_realistic_v14-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=8,
+    n_events=3_576_000,
+    aux={
+        "merging_factors": {
+            "nominal": 14,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="zzz_amcatnlo",
+    id=14808802,
+    processes=[procs.zzz],
+    keys=[
+        "/ZZZ_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer23MiniAODv4_NanoAODv14UHH-130X_mcRun3_2023_realistic_v14-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=7,
+    n_events=3_600_000,
+    aux={
+        "merging_factors": {
+            "nominal": 16,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="wwz_4f_amcatnlo",
+    id=14808799,
+    processes=[procs.wwz],
+    keys=[
+        "/WWZ_4F_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer23MiniAODv4_NanoAODv14UHH-130X_mcRun3_2023_realistic_v14-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=8,
+    n_events=3_600_000,
+    aux={
+        "merging_factors": {
+            "nominal": 14,
+        },
+    },
+)
 
 # tba
