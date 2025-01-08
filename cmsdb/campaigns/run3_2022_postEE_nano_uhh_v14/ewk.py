549536034fdd0b20efbddc1adf4403a3713161ab
Marcel R. <github.riga@icloud.com>
2025-01-08 08:38:17 +0100

    Add di-/triple-boson and tt+boson datasets to 22post_v14.

--- cmsdb/campaigns/run3_2022_postEE_nano_uhh_v14/ewk.py
+++ cmsdb/campaigns/run3_2022_postEE_nano_uhh_v14/ewk.py
@@ -5,8 +5,8 @@ Electroweak datasets for the 2022 postEE data-taking campaign with datasets at N
 version 14, created with custom content at UHH.
 """
 
-# import cmsdb.processes as procs
-# from cmsdb.campaigns.run3_2022_postEE_nano_uhh_v14 import campaign_run3_2022_postEE_nano_uhh_v14 as cpn
+import cmsdb.processes as procs
+from cmsdb.campaigns.run3_2022_postEE_nano_uhh_v14 import campaign_run3_2022_postEE_nano_uhh_v14 as cpn
 
 
 #
@@ -31,10 +31,118 @@ version 14, created with custom content at UHH.
 # Di-boson
 #
 
-# tba
+cpn.add_dataset(
+    name="ww_pythia",
+    id=14800910,
+    processes=[procs.ww],
+    keys=[
+        "/WW_TuneCP5_13p6TeV_pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=61,
+    n_events=53_112_080,
+    aux={
+        "merging_factors": {
+            "nominal": 12,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="wz_pythia",
+    id=14799468,
+    processes=[procs.wz],
+    keys=[
+        "/WZ_TuneCP5_13p6TeV_pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=33,
+    n_events=26_722_782,
+    aux={
+        "merging_factors": {
+            "nominal": 12,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="zz_pythia",
+    id=14798176,
+    processes=[procs.zz],
+    keys=[
+        "/ZZ_TuneCP5_13p6TeV_pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=5,
+    n_events=4_043_040,
+    aux={
+        "merging_factors": {
+            "nominal": 17,
+        },
+    },
+)
 
 #
 # Triple-boson
 #
 
-# tba
+cpn.add_dataset(
+    name="www_4f_amcatnlo",
+    id=14791571,
+    processes=[procs.www],
+    keys=[
+        "/WWW_4F_TuneCP5_13p6TeV_amcatnlo-madspin-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=3,
+    n_events=1_482_480,
+    aux={
+        "merging_factors": {
+            "nominal": 20,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="wwz_4f_amcatnlo",
+    id=14792375,
+    processes=[procs.wwz],
+    keys=[
+        "/WWZ_4F_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=12,
+    n_events=5_771_216,
+    aux={
+        "merging_factors": {
+            "nominal": 11,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="wzz_amcatnlo",
+    id=14794158,
+    processes=[procs.wzz],
+    keys=[
+        "/WZZ_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=12,
+    n_events=5_751_288,
+    aux={
+        "merging_factors": {
+            "nominal": 11,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="zzz_amcatnlo",
+    id=14803298,
+    processes=[procs.zzz],
+    keys=[
+        "/ZZZ_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=11,
+    n_events=5_872_560,
+    aux={
+        "merging_factors": {
+            "nominal": 12,
+        },
+    },
+)
--- cmsdb/campaigns/run3_2022_postEE_nano_uhh_v14/top.py
+++ cmsdb/campaigns/run3_2022_postEE_nano_uhh_v14/top.py
@@ -7,8 +7,8 @@ version 14, created with custom content at UHH.
 
 # from order import DatasetInfo
 
-# import cmsdb.processes as procs
-# from cmsdb.campaigns.run3_2022_postEE_nano_uhh_v14 import campaign_run3_2022_postEE_nano_uhh_v14 as cpn
+import cmsdb.processes as procs
+from cmsdb.campaigns.run3_2022_postEE_nano_uhh_v14 import campaign_run3_2022_postEE_nano_uhh_v14 as cpn
 
 
 #
@@ -21,13 +21,137 @@ version 14, created with custom content at UHH.
 # ttbar + 1 vector boson
 #
 
-# tba
+cpn.add_dataset(
+    name="ttw_wlnu_amcatnlo",
+    id=14850108,
+    processes=[procs.ttw_wlnu],
+    keys=[
+        "/TTLNu-1Jets_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v4/NANOAODSIM",  # noqa
+    ],
+    n_files=1,
+    n_events=335_985,
+    aux={
+        "merging_factors": {
+            "nominal": 10,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="ttz_zqq_amcatnlo",
+    id=14792049,
+    processes=[procs.ttz_zqq],
+    keys=[
+        "/TTZ-ZtoQQ-1Jets_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=4,
+    n_events=1_330_390,
+    aux={
+        "merging_factors": {
+            "nominal": 17,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="ttz_zll_m4to50_amcatnlo",
+    id=14790944,
+    processes=[procs.ttz_zll_m4to50],
+    keys=[
+        "/TTLL_MLL-4to50_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=3,
+    n_events=1_049_999,
+    aux={
+        "merging_factors": {
+            "nominal": 12,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="ttz_zll_m50toinf_amcatnlo",
+    id=14791092,
+    processes=[procs.ttz_zll_m50toinf],
+    keys=[
+        "/TTLL_MLL-50_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=5,
+    n_events=1_386_140,
+    aux={
+        "merging_factors": {
+            "nominal": 9,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="ttz_znunu_amcatnlo",
+    id=14881355,
+    processes=[procs.ttz_znunu],
+    keys=[
+        "/TTNuNu_TuneCP5_13p6TeV_amcatnlo-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=5,
+    n_events=1_746_360,
+    aux={
+        "merging_factors": {
+            "nominal": 16,
+        },
+    },
+)
 
 #
 # ttbar + 2 vector bosons
 #
 
-# tba
+cpn.add_dataset(
+    name="ttww_madgraph",
+    id=14797703,
+    processes=[procs.ttww],
+    keys=[
+        "/TTWW_TuneCP5_13p6TeV_madgraph-madspin-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=5,
+    n_events=1_536_000,
+    aux={
+        "merging_factors": {
+            "nominal": 15,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="ttwz_madgraph",
+    id=15007010,
+    processes=[procs.ttwz],
+    keys=[
+        "/TTWZ_TuneCP5_13p6TeV_madgraph-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=5,
+    n_events=1_500_000,
+    aux={
+        "merging_factors": {
+            "nominal": 20,
+        },
+    },
+)
+
+cpn.add_dataset(
+    name="ttzz_madgraph",
+    id=14826274,
+    processes=[procs.ttzz],
+    keys=[
+        "/TTZZ_TuneCP5_13p6TeV_madgraph-madspin-pythia8/Run3Summer22EEMiniAODv4_NanoAODv14UHH-130X_mcRun3_2022_realistic_postEE_v6-v3/NANOAODSIM",  # noqa
+    ],
+    n_files=5,
+    n_events=1_519_000,
+    aux={
+        "merging_factors": {
+            "nominal": 9,
+        },
+    },
+)
 
 #
 # single top
