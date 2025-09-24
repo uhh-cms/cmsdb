86a4dd9e529d8ecc989d1ea71d8301810f3c6ffe
Marcel R. <github.riga@icloud.com>
2025-09-24 11:08:46 +0200

    Add missing k2v 0 sample in 23pre.

--- cmsdb/campaigns/run3_2023_preBPix_nano_uhh_v14/hh2bbtautau.py
+++ cmsdb/campaigns/run3_2023_preBPix_nano_uhh_v14/hh2bbtautau.py
@@ -176,7 +176,21 @@ cpn.add_dataset(
     },
 )
 
-# TODO: k2v0 sample still being centrally processed
+cpn.add_dataset(
+    name="hh_vbf_hbb_htt_kv1_k2v0_kl1_madgraph",
+    id=15377237,
+    processes=[procs.hh_vbf_hbb_htt_kv1_k2v0_kl1],
+    keys=[
+        "/VBFHHto2B2Tau_CV_1_C2V_0_C3_1_TuneCP5_13p6TeV_madgraph-pythia8/Run3Summer23MiniAODv4_NanoAODv14UHH-130X_mcRun3_2023_realistic_v15-v3/NANOAODSIM",  # noqa
+    ],
+    n_files=7,
+    n_events=2_666_667,
+    aux={
+        "merging_factors": {
+            "nominal": 13,
+        },
+    },
+)
 
 cpn.add_dataset(
     name="hh_vbf_hbb_htt_kv1p74_k2v1p37_kl14p4_madgraph",
