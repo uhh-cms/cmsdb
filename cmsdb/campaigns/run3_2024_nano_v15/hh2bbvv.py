241febec2eeb0f57c218540b8eca387966b7e668
Balduin <balduin.letzer@studium.uni-hamburg.de>
2026-03-04 13:47:22 +0100

    add avail. 24 HH procs, add dym10to50 as parent to dy_ll_m10to50 procs

--- cmsdb/campaigns/run3_2024_nano_v15/hh2bbvv.py
+++ cmsdb/campaigns/run3_2024_nano_v15/hh2bbvv.py
@@ -4,8 +4,6 @@
 HH -> bbVV datasets for the 2024 data-taking campaign with datasets at NanoAOD tier in version 15.
 """
 
-from order import DatasetInfo
-
 import cmsdb.processes as procs
 from cmsdb.campaigns.run3_2024_nano_v15 import campaign_run3_2024_nano_v15 as cpn
 
@@ -14,64 +12,120 @@ from cmsdb.campaigns.run3_2024_nano_v15 import campaign_run3_2024_nano_v15 as cp
 # ggf -> HH
 #
 
-# todo
+cpn.add_dataset(
+    name="hh_ggf_hbb_hvv2l2nu_kl1_kt1_powheg",
+    id=15436982,
+    processes=[procs.hh_ggf_hbb_hvv2l2nu_kl1_kt1],
+    keys=[
+        "/GluGlutoHHto2B2Vto2L2Nu_Par-c2-0p00-kl-1p00-kt-1p00_TuneCP5_13p6TeV_powheg-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
+    ],
+    aux={
+        "broken_files": [],
+    },
+    n_files=155,  # 155-0
+    n_events=1174326,
+)
+
+cpn.add_dataset(
+    name="hh_ggf_hbb_hvv2l2nu_kl2p45_kt1_powheg",
+    id=15432921,
+    processes=[procs.hh_ggf_hbb_hvv2l2nu_kl2p45_kt1],
+    keys=[
+        "/GluGlutoHHto2B2Vto2L2Nu_Par-c2-0p00-kl-2p45-kt-1p00_TuneCP5_13p6TeV_powheg-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
+    ],
+    aux={
+        "broken_files": [],
+    },
+    n_files=260,  # 260-0
+    n_events=1172869,
+)
 
 #
 # vbf -> HH
 #
 
 cpn.add_dataset(
+    name="hh_vbf_hbb_hvv2l2nu_kv1_k2v1_kl1_madgraph",
+    id=15432384,
+    processes=[procs.hh_vbf_hbb_hvv2l2nu_kv1_k2v1_kl1],
+    keys=[
+        "/VBFHHto2B2Vto2L2Nu_Par-CV-1-C2V-1-C3-1_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
+    ],
+    aux={
+        "broken_files": [],
+    },
+    n_files=31,  # 31-0
+    n_events=1199996,
+)
+
+cpn.add_dataset(
     name="hh_vbf_hbb_hvv2l2nu_kv2p12_k2v3p87_klm5p96_madgraph",
     id=15437989,
     processes=[procs.hh_vbf_hbb_hvv2l2nu_kv2p12_k2v3p87_klm5p96],
-    info=dict(
-        nominal=DatasetInfo(
-            keys=[
-                "/VBFHHto2B2Vto2L2Nu_Par-CV-2p12-C2V-3p87-C3-m5p96_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
-            ],
-            aux={
-                "broken_files": [],
-            },
-            n_files=11,  # 11-0
-            n_events=1196998,
-        ),
-    ),
+    keys=[
+        "/VBFHHto2B2Vto2L2Nu_Par-CV-2p12-C2V-3p87-C3-m5p96_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
+    ],
+    aux={
+        "broken_files": [],
+    },
+    n_files=11,  # 11-0
+    n_events=1196998,
 )
 
 cpn.add_dataset(
     name="hh_vbf_hbb_hvv2l2nu_kvm0p758_k2v1p44_klm19p3_madgraph",
     id=15436549,
     processes=[procs.hh_vbf_hbb_hvv2l2nu_kvm0p758_k2v1p44_klm19p3],
-    info=dict(
-        nominal=DatasetInfo(
-            keys=[
-                "/VBFHHto2B2Vto2L2Nu_Par-CV-m0p758-C2V-1p44-C3-m19p3_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
-            ],
-            aux={
-                "broken_files": [],
-            },
-            n_files=11,  # 11-0
-            n_events=1199997,
-        ),
-    ),
+    keys=[
+        "/VBFHHto2B2Vto2L2Nu_Par-CV-m0p758-C2V-1p44-C3-m19p3_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
+    ],
+    aux={
+        "broken_files": [],
+    },
+    n_files=11,  # 11-0
+    n_events=1199997,
 )
 
 cpn.add_dataset(
     name="hh_vbf_hbb_hvv2l2nu_kvm0p962_k2v0p959_klm1p43_madgraph",
     id=15435128,
     processes=[procs.hh_vbf_hbb_hvv2l2nu_kvm0p962_k2v0p959_klm1p43],
-    info=dict(
-        nominal=DatasetInfo(
-            keys=[
-                "/VBFHHto2B2Vto2L2Nu_Par-CV-m0p962-C2V-0p959-C3-m1p43_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
-            ],
-            aux={
-                "broken_files": [],
-            },
-            n_files=25,  # 25-0
-            n_events=1198995,
-        ),
-    ),
+    keys=[
+        "/VBFHHto2B2Vto2L2Nu_Par-CV-m0p962-C2V-0p959-C3-m1p43_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
+    ],
+    aux={
+        "broken_files": [],
+    },
+    n_files=25,  # 25-0
+    n_events=1198995,
+)
+
+cpn.add_dataset(
+    name="hh_vbf_hbb_hvv2l2nu_kvm1p6_k2v2p72_klm1p36_madgraph",
+    id=15468816,
+    processes=[procs.hh_vbf_hbb_hvv2l2nu_kvm1p6_k2v2p72_klm1p36],
+    keys=[
+        "/VBFHHto2B2Vto2L2Nu_Par-CV-m1p60-C2V-2p72-C3-m1p36_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
+    ],
+    aux={
+        "broken_files": [],
+    },
+    n_files=39,  # 39-0
+    n_events=1199997,
+)
+
+cpn.add_dataset(
+    name="hh_vbf_hbb_hvv2l2nu_kvm1p83_k2v3p57_klm3p39_madgraph",
+    id=15445359,
+    processes=[procs.hh_vbf_hbb_hvv2l2nu_kvm1p83_k2v3p57_klm3p39],
+    keys=[
+        "/VBFHHto2B2Vto2L2Nu_Par-CV-m1p83-C2V-3p57-C3-m3p39_TuneCP5_13p6TeV_madgraph-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa: E501
+    ],
+    aux={
+        "broken_files": [],
+    },
+    n_files=33,  # 33-0
+    n_events=1199998,
 )
 
 #
