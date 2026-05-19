6e418a284e28d24abad06985d84f305494b78d9c
Lukas Ebeling <lukas.ebeling@uni-hamburg.de>
2026-05-19 13:40:06 +0200

    4 top sample

--- cmsdb/campaigns/run3_2024_nano_v15/top.py
+++ cmsdb/campaigns/run3_2024_nano_v15/top.py
@@ -610,8 +610,13 @@ cpn.add_dataset(
 # 4 top
 #
 
-# missing
-# cpn.add_dataset(
-#     name="tttt_amcatnlo",
-#     ...
-# )
+cpn.add_dataset(
+    name="tttt_amcatnlo",
+    id=15529183,
+    processes=[procs.tttt],
+    keys=[
+        "/TTTT_TuneCP5_13p6TeV_amcatnlo-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM",  # noqa
+    ],
+    n_files=101,  # 101-0
+    n_events=9776800,
+)
